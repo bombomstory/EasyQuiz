513e1421b29d3cd4092af43449fd675ea806fee2
bombomstory <bombomstory@gmail.com>
2024-05-03 17:07:38 +0700

    เพิ่มข้อสอบ

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-function checkAnswer(answer) {
-    const result = document.getElementById('result');
-    if (answer === 'a') {
-        result.textContent = "ถูกต้อง PIM ย่อมาจาก PIM";
-        result.style.color = 'green';
+var totalscore = 0;
+
+function checkAnswer(question, answer) {
+    const result1 = document.getElementById('result1');
+    const result2 = document.getElementById('result2');
+
+    if (question == 'question1'){
+        if (answer === 'a') {
+            result1.textContent = "ถูกต้อง PIM ย่อมาจาก PIM";
+            result1.style.color = 'green';
+            totalscore = totalscore + 1;
+            summary.textContent = "คุณได้คะแนนรวม: "+totalscore+" คะแนน";
+            summary.style.color = 'blue';
+        } else {
+            result1.textContent = "Incorrect! Try again.";
+            result1.style.color = 'red';
+            summary.textContent = "คุณได้คะแนนรวม: "+totalscore+" คะแนน";
+            summary.style.color = 'red';
+        }
+    } else if (question == 'question2') {
+        if (answer === 'b') {
+            result2.textContent = "ชื่อย่อของมหาวิทยาลัยกาฬสินธุ์คือ KSU";
+            result2.style.color = 'green';
+            totalscore = totalscore + 1;
+            summary.textContent = "คุณได้คะแนนรวม: "+totalscore+" คะแนน";
+            summary.style.color = 'blue';
+        } else {
+            result2.textContent = "Incorrect! Try again.";
+            result2.style.color = 'red';
+            summary.textContent = "คุณได้คะแนนรวม: "+totalscore+" คะแนน";
+            summary.style.color = 'red';
+        }
     } else {
-        result.textContent = "Incorrect! Try again.";
-        result.style.color = 'red';
+        // Nothing
     }
+
 }
